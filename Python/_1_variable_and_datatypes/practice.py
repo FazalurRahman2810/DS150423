1229d9b7b7bfac77cf5ad87742df142e1c78da47
FazalurRahman2810 <131470471+FazalurRahman2810@users.noreply.github.com>
2023-04-23 08:32:00 -0700

    Create practice.py

---
+++ Python/_1_variable_and_datatypes/practice.py
@@ -0,0 +1,11 @@
+english = 60
+maths = 50
+science = 70
+sub1 =int(input("english : "))
+sub2 =int(input("math's : "))
+sub3 =int(input("science : "))
+print()
+total = sub1+sub2+sub3
+print("total score: ", total)
+percentage =(total/300)*100
+print("percentage = ", percentage,"%")
